332bb060f54d9dc30b124308c3423b41d13f4cc0
Brandon Wortman <bwortman@umich.edu>
2023-05-28 14:48:21 -0400

    Need to test cocktail menu Ran formatter

--- src/Machine.hpp
+++ src/Machine.hpp
@@ -13,9 +13,10 @@
 const size_t MOTOR_COUNT = 8;
 const size_t BEV_COUNT = 12;
 
-const size_t PASSCODE[] = {1,0,2,5}; //ik im not proud of this 
+const size_t PASSCODE[] = {1, 0, 2, 5}; // ik im not proud of this
 
-struct Bottle {
+struct Bottle
+{
     String name;
     int id = -1;
     bool active = false;
@@ -24,83 +25,89 @@ struct Bottle {
     double estimatedCapacity = 0.0;
 };
 
+class TouchControl
+{
+private:
+    void touchOutput(String str); // touch screen requires specific format
+    void flushStream()
+    {
+        Serial2.write(0xff);
+        Serial2.write(0xff);
+        Serial2.write(0xff);
+    };
 
-
-class TouchControl {
-    private:
-        void touchOutput(String str); //touch screen requires specific format
-        void flushStream() {
-            Serial2.write(0xff);
-            Serial2.write(0xff);
-            Serial2.write(0xff);
-        };
-    public:
+public:
     /*
-        TODO: create a function that abstracts the controlling of screen, like buttonId, etc. 
+        TODO: create a function that abstracts the controlling of screen, like buttonId, etc.
     */
-        String checkForInput();
-        int currentPage = 0;
+    String checkForInput();
+    int currentPage = 0;
 
-        void printDebug(String str) {
-            Serial.print("SENT T.SCREEN: ");
-            Serial.println(str);
-            touchOutput(str);
-        }; //will print to both console serial and to lcd serial
-        void print(String str) {
-            touchOutput(str);
-        }; //void print()
-        /* void controlCurPage:
-        Follow Nextion guidelines.
-        */
-        void controlCurPage(String item, String cmd, String val);
-        void changePage(String pageNum) {
-            flushStream();
-            String send = "page ";
-            send.concat(pageNum);
-            touchOutput(send);
-            currentPage = pageNum.toInt();
-        }; //void changePage()
+    void printDebug(String str)
+    {
+        Serial.print("SENT T.SCREEN: ");
+        Serial.println(str);
+        touchOutput(str);
+    }; // will print to both console serial and to lcd serial
+    void print(String str)
+    {
+        touchOutput(str);
+    }; // void print()
+    /* void controlCurPage:
+    Follow Nextion guidelines.
+    */
+    void controlCurPage(String item, String cmd, String val);
+    void changePage(String pageNum)
+    {
+        flushStream();
+        String send = "page ";
+        send.concat(pageNum);
+        touchOutput(send);
+        currentPage = pageNum.toInt();
+    }; // void changePage()
 
-}; //class TouchControl
+}; // class TouchControl
 
-class Beverage {
-    private:
-    public:
-        String name;
-        bool isActive = true;
-        double ozArr[MOTOR_COUNT];
-        String additionalInstructions = "";
-        void createBeverage(std::vector<Bottle> &bottles);
+class Beverage
+{
+private:
+public:
+    String name;
+    int id = -1;
+    bool isActive = true;
+    double ozArr[MOTOR_COUNT];
+    String additionalInstructions = "";
+    void createBeverage(std::vector<Bottle> &bottles);
 };
 
-class Machine {
-    private:
-        //SD CARD
-        File config;
-        File data;
-        File log;
-        //File paymentLog
+class Machine
+{
+private:
+    // SD CARD
+    File config;
+    File data;
+    File log;
+    // File paymentLog
 
-        std::vector<Bottle> bottles;
-        std::vector<Beverage> beverages;
-        bool authCocktail = false; //require auth on cocktail
-        bool authShots = false; //require auth on shots
-        
-        //filedata
-        void initSD();
-        void initConfig();
-        void initBottles();
-        void initData();
+    std::vector<Bottle> bottles;
+    std::vector<Beverage> beverages;
+    bool authCocktail = false; // require auth on cocktail
+    bool authShots = false;    // require auth on shots
 
-        void loadMainMenu();
-        void loadAdminMenu();
+    // filedata
+    void initSD();
+    void initConfig();
+    void initBottles();
+    void initData();
 
-    public:
-        TouchControl touchscreen;
-        void boot();
-        void makeSelection();
-        
+    void loadMainMenu();
+    void loadAdminMenu();
+    void loadCocktailMenu();
+
+public:
+    TouchControl touchscreen;
+    void boot();
+    void makeSelection();
 };
-
 
 #endif /*Machine_hpp*/
